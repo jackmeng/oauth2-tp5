3b2ccca338c3d3d07b649dd36ecc32ca94c46278
jackmeng <jiekemeng@gmail.com>
2018-03-29 18:16:47 +0800

    first commit

---
+++ src/App.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jack
+ * Date: 2018/3/29
+ * Time: 18:00
+ */
+
+namespace Jackmeng\Oauth2;
+
+
+class App
+{
+    public function lists()
+    {
+
+    }
+
+    public function read()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function stop()
+    {
+
+    }
+
+    public function recover()
+    {
+
+    }
+
+    public function check()
+    {
+
+    }
+
+    public function setSecret()
+    {
+
+    }
+}
---
+++ src/Auth.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jack
+ * Date: 2018/3/29
+ * Time: 18:00
+ */
+
+namespace Jackmeng\Oauth2;
+
+
+class Auth
+{
+    public function getCode()
+    {
+
+    }
+
+    public function getToken()
+    {
+
+    }
+
+    public function refreshToken()
+    {
+
+    }
+
+    public function read()
+    {
+
+    }
+}
